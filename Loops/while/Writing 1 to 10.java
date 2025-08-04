b73d32c1f1372da3ddc12efcdb1201f4ffe00d49
Priyanka Patel <patel.priyanka2605@gmail.com>
2025-08-04 18:38:05 +0530

    Create Writing 1 to 10.java

---
+++ Loops/while/Writing 1 to 10.java
@@ -0,0 +1,11 @@
+public class Loops{
+    public static void main(String[] args) {
+        int i=0;
+        while (i<11) { 
+            System.out.println(i);
+            i=i+1;
+
+        }
+        }
+
+    }
