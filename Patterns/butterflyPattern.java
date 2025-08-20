f2304d5f401da3df74e6ef190bfcbe4b8400ee0f
Priyanka Patel <patel.priyanka2605@gmail.com>
2025-08-20 17:44:22 +0530

    Create butterflyPattern.java

---
+++ Patterns/butterflyPattern.java
@@ -0,0 +1,48 @@
+class Main{
+    public static void main(String[] args){
+        int n=4;
+        //upper half
+        for(int i=1;i<=n;i++){
+            //first part stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //spaces
+            int spaces=2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //second part stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //lower half
+        for(int i=n;i>=1;i--){
+            //first part stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            //spaces
+            int spaces=2*(n-i);
+            for(int j=1;j<=spaces;j++){
+                System.out.print(" ");
+            }
+            //second part stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+//Output-
+//*      *
+//**    **
+//***  ***
+//********
+//********
+//***  ***
+//**    **
+//*      *
