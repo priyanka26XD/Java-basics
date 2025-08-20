142761b13dae2ccbabb411aaa79610fb0fb60000
Priyanka Patel <patel.priyanka2605@gmail.com>
2025-08-20 17:49:02 +0530

    Create Factorial.java

---
+++ Functions/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Functions{
+    public static void printFactorial(int num){
+        if(num<0){
+            System.out.println("Invalid number");
+            return;
+        }
+        int factorial=1;
+        for(int i=num;i>=1;i--){
+            factorial=factorial*i;
+        }
+        System.out.println("Factorial of the number is:"+factorial);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+
+        printFactorial(num);
+    }
+}
